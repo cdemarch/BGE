b16a778c8f9fba648e94a7691973d142c244d4af
ChrisDeMarchi <skyrim44445@gmail.com>
2024-10-20 17:53:26 -0400

    trying

--- backend/models/user.js
+++ backend/models/user.js
@@ -1,80 +1,37 @@
+// models/user.js
 const mongoose = require('mongoose');
-const bcrypt = require('bcryptjs')
-const validator = require('validator');
-const Schema = mongoose.Schema
+const bcrypt = require('bcrypt');
 
+const userSchema = new mongoose.Schema({
+  username: { type: String, required: true, unique: true },
+  password: { type: String, required: true },
+  firstName: { type: String, required: true },
+  lastName: { type: String, required: true },
+});
 
-const userSchema = new Schema({
-  firstName: {
-    type: String,
-    required: [true, 'First name is required'],
-    minlength: [2, 'First name must be longer than 2 characters'],
-    maxlength: [50, 'First name cannot be longer than 50 characters'],
-  },
-  lastName: {
-    type: String,
-    required: [true, 'Last name is required'],
-    minlength: [2, 'Last name must be longer than 2 characters'],
-    maxlength: [50, 'Last name cannot be longer than 50 characters'],
-  },
-  username: {
-    type: String,
-    required: [true, 'Username is required'],
-    unique: [true, 'Username must be unique'],
-    minlength: [2, 'Username must be longer than 2 characters'],
-    maxlength: [50, 'Username cannot be longer than 50 characters'],
-  },
-  password: {
-    type: String,
-    required: [true, 'Password is required'],
-    minlength: [6, 'Password must be longer than 6 characters'],
-  },
-})
+// Signup static method
+userSchema.statics.signup = async function(username, password, firstName, lastName) {
+  // Check if username already exists
+  const exists = await this.findOne({ username });
+  if (exists) throw Error('Username already in use.');
 
-// Static signup method
-userSchema.statics.signup = async function (username, password, firstName, lastName){
-  // Validation
-  if (!username || !password || !firstName || !lastName) {
-    throw Error("All fields must be filled in!")
-  }
-  if (!validator.isStrongPassword(password)) {
-    throw Error("Password is not strong enough!")
-  }
-  // Check if username exists
-  const exists = await this.findOne({ username })
-
-  if (exists) {
-    throw Error('Username already in use!');
-  }
-
-  // Salt and Hash password
+  // Hash password
   const salt = await bcrypt.genSalt(10);
   const hash = await bcrypt.hash(password, salt);
 
-  return await this.create({username, password: hash, firstName, lastName});
-}
+  const user = await this.create({ username, password: hash, firstName, lastName });
+  return user;
+};
 
-// Static login method
-userSchema.statics.login = async function (username, password) {
-  if (!username || !password) {
-    throw Error("All fields must be filled in!")
-  }
+// Login static method
+userSchema.statics.login = async function(username, password) {
+  const user = await this.findOne({ username });
+  if (!user) throw Error('Incorrect username.');
 
-  // Check if username exists
-  const user = await this.findOne({ username })
-
-  if (!user) {
-    throw Error('Incorrect username!');
-  }
-
-  // Match passwords
   const match = await bcrypt.compare(password, user.password);
-
-  if (!match) {
-    throw Error('Incorrect password!')
-  }
+  if (!match) throw Error('Incorrect password.');
 
   return user;
-}
+};
 
 module.exports = mongoose.model('User', userSchema);
--- frontend/src/components/NavigationBar.jsx
+++ frontend/src/components/NavigationBar.jsx
@@ -1,25 +1,26 @@
 import React from "react";
-import {Button, Container, Nav, Navbar} from "react-bootstrap";
+import { Button, Container, Nav, Navbar } from "react-bootstrap";
+import { Link } from "react-router-dom"; // Import Link
 
 const NavBar = () => {
   return (
-    <Navbar expand="lg" bg="dark" variant="dark">
-      <Container>
-        <Navbar.Brand href="/">BGE</Navbar.Brand>
-        <Navbar.Collapse id="basic-navbar-nav">
-          <Nav className="me-auto">
-            <Nav.Link href="/">Home</Nav.Link>
-            <Nav.Link href="/about">About</Nav.Link>
-            <Nav.Link href="/contact">Contact</Nav.Link>
-          </Nav>
-          {/* Login Button*/}
-          <Nav>
-            <Button variant="outline-light" href="/login">Login</Button>
-          </Nav>
-        </Navbar.Collapse>
-      </Container>
-    </Navbar>
-  )
+      <Navbar expand="lg" bg="dark" variant="dark">
+        <Container>
+          <Navbar.Brand as={Link} to="/">BGE</Navbar.Brand> {/* Use Link for navigation */}
+          <Navbar.Collapse id="basic-navbar-nav">
+            <Nav className="me-auto">
+              <Nav.Link as={Link} to="/">Home</Nav.Link> {/* Use Link for navigation */}
+              <Nav.Link as={Link} to="/about">About</Nav.Link> {/* Use Link for navigation */}
+              <Nav.Link as={Link} to="/contact">Contact</Nav.Link> {/* Use Link for navigation */}
+            </Nav>
+            {/* Login Button*/}
+            <Nav>
+              <Button variant="outline-light" as={Link} to="/login">Login</Button> {/* Use Link for navigation */}
+            </Nav>
+          </Navbar.Collapse>
+        </Container>
+      </Navbar>
+  );
 }
 
-export default NavBar
+export default NavBar;
